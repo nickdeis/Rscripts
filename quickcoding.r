58f94d384f305991489fbe29bd7459e3111fd461
Nicholas Deis <nickjdeis@gmail.com>
2013-06-02 20:37:25 -0400

    Add something to the message

--- quickcoding.r
+++ quickcoding.r
@@ -1,8 +1,10 @@
 #A simple program to making human based coding eaiser
 #easycode_string(dataframe,string,string)
 easycode <- function(X,data_column,coding_column,auto_fill){
+
 	keep_going=TRUE #Stops the program when you are done coding, another way to stop is the ctrl+c
 	while(keep_going){
+	
 	
 		for( i in 1:length( X[, data_column ] ) ) {
 			if( is.na( X[i, data_column ] ) ){
@@ -17,7 +19,7 @@ easycode <- function(X,data_column,coding_column,auto_fill){
 		}
 		
 		#Section where I ask things to be parsed#
-		parser=readline("Is there a specific format you would like the data you just coded to be in? type int for integers or dates for calendar dates written in integer form")
+		parser=readline("Is there a specific format you would like the data you just coded to be in? type int for integers or dates for calendar dates written in integer form. If not, just press enter")
 		#if yes#
 		if(parser == "int"){
 			X[,coding_column]=as.integer(X[,coding_column])
